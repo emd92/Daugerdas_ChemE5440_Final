02a8e3aec6c26b0c103c6b11030c501675aa376b
emd92 <61260776+emd92@users.noreply.github.com>
2020-05-22 10:35:05 -0500

    Update P1.B.jl

--- P1.B.jl
+++ P1.B.jl
@@ -1,7 +1,7 @@
 #Problem 1: Part B
 include("PhasePortraitV2.jl")
 function ODE_Final(D1,D2)
-  v = 0.00000000001
+
   deltaD1 = @. (1/(1+10*(D2^2/(0.1+D2^2))^2))-D1
   deltaD2 = @. (1/(1+10*(D1^2/(0.1+D1^2))^2))-D2
   
@@ -10,10 +10,7 @@ end
 
 tspan = (0.0,50.0)
 
-xlim = (0, 1, 20)
-ylim = (0, 1, 20)
+xlim = (0, 1, 30)
+ylim = (0, 1, 30)
 
-#Show that in the long-time limit, the system will settle into a steady state
-x0=([0.3,0.32],[0.3,0.32])
-
-phaseplot(ODE_Final,xlim,ylim,xinit=x0,clines=true,t=tspan,norm=true,scale=0.5)
+phaseplot(ODE_Final,xlim,ylim,clines=true,t=tspan,norm=true,scale=0.5)
